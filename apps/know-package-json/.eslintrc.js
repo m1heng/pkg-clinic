89b0ba7eb5efb0198b19bc14cba5973b72913e55
m1heng <m1heng@me.com>
2021-12-05 17:02:55 +0800

    housekeeping

--- apps/know-package-json/.eslintrc.js
+++ apps/know-package-json/.eslintrc.js
@@ -1,16 +1,11 @@
-
 module.exports = {
   extends: [
     "next/core-web-vitals", "@pkgc/eslint-config/profiles/base"
   ],
-  parserOptions: { tsconfigRootDir: __dirname },
   "settings": {
     "next": {
       "rootDir": __dirname
     }
-  },
-  rules: {
-    
   },
   root: true
 };
